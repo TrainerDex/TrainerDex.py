730e04429d87f9c00433d0642b3201775062952b
Jay Turner <jaynicholasturner@gmail.com>
2017-09-11 18:41:43 +0100

    Added `trainerLevels`

--- TrainerDex.py
+++ TrainerDex.py
@@ -88,11 +88,72 @@ Server = namedtuple('Server', [
 	'owner',
 ])
 
+Level = namedtuple('Level', [
+	'level',
+	'total_xp',
+	'xp_required'
+])
+
 class Requests:
 	def __init__(self, token):
 		self.url = 'http://127.0.0.1:8000/api/trainer/'
 		self.token = token
 		self.headers = {'content-type':'application/json', 'authorization':'Token '+token}
+		
+	def trainerLevels(self, xp=None, level=None):
+		levels = [
+			Level(level=1,total_xp=0,xp_required=0),
+			Level(level=2,total_xp=1000,xp_required=1000),
+			Level(level=3,total_xp=3000,xp_required=2000),
+			Level(level=4,total_xp=6000,xp_required=3000),
+			Level(level=5,total_xp=10000,xp_required=4000),
+			Level(level=6,total_xp=15000,xp_required=5000),
+			Level(level=7,total_xp=21000,xp_required=6000),
+			Level(level=8,total_xp=18000,xp_required=7000),
+			Level(level=9,total_xp=36000,xp_required=8000),
+			Level(level=10,total_xp=45000,xp_required=9000),
+			Level(level=11,total_xp=55000,xp_required=10000),
+			Level(level=12,total_xp=65000,xp_required=10000),
+			Level(level=13,total_xp=75000,xp_required=10000),
+			Level(level=14,total_xp=85000,xp_required=10000),
+			Level(level=15,total_xp=100000,xp_required=15000),
+			Level(level=16,total_xp=120000,xp_required=20000),
+			Level(level=17,total_xp=140000,xp_required=20000),
+			Level(level=18,total_xp=160000,xp_required=20000),
+			Level(level=19,total_xp=185000,xp_required=25000),
+			Level(level=20,total_xp=210000,xp_required=25000),
+			Level(level=21,total_xp=260000,xp_required=50000),
+			Level(level=22,total_xp=335000,xp_required=75000),
+			Level(level=23,total_xp=456000,xp_required=100000),
+			Level(level=24,total_xp=560000,xp_required=125000),
+			Level(level=25,total_xp=710000,xp_required=150000),
+			Level(level=26,total_xp=900000,xp_required=190000),
+			Level(level=27,total_xp=1100000,xp_required=200000),
+			Level(level=28,total_xp=1350000,xp_required=250000),
+			Level(level=29,total_xp=1650000,xp_required=300000),
+			Level(level=30,total_xp=2000000,xp_required=350000),
+			Level(level=31,total_xp=2500000,xp_required=500000),
+			Level(level=32,total_xp=3000000,xp_required=500000),
+			Level(level=33,total_xp=3750000,xp_required=750000),
+			Level(level=34,total_xp=4750000,xp_required=1000000),
+			Level(level=35,total_xp=6000000,xp_required=1250000),
+			Level(level=36,total_xp=7500000,xp_required=1500000),
+			Level(level=37,total_xp=9500000,xp_required=2000000),
+			Level(level=38,total_xp=12000000,xp_required=2500000),
+			Level(level=39,total_xp=15000000,xp_required=3000000),
+			Level(level=40,total_xp=20000000,xp_required=5000000)
+		]
+		if level is not None:
+			for t in levels:
+				if t.level==level:
+					return t.total_xp
+		elif xp is not None:
+			for t in levels:
+				if t.total_xp<=xp:
+					level = t.level
+			return level
+		else:
+			return levels		
 	
 	def getTrainer(self, name, force=False):
 		r = requests.get(self.url+'trainers/'+name+'/').json()
