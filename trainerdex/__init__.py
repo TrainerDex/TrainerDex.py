ee0723d51242313aaa738f0446bd0f0dc034ac1f
Jay Turner <jaynicholasturner@gmail.com>
2017-12-11 00:04:10 +0000

    Update api_url

--- trainerdex/__init__.py
+++ trainerdex/__init__.py
@@ -15,7 +15,7 @@ __title__ = 'trainerdex'
 __author__ = 'JayTurnr'
 __licence__ = 'GNU-GPL'
 __copyright__ = 'Copyright 2017 JayTurnr'
-__version__ = '1.4.2'
+__version__ = '1.4.2.1'
 
 from .client import Client
 from .trainer import Trainer
