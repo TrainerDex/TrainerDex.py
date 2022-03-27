66f695806480919c76fa4c62618c9c2cc5732091
Jay Turner <jay@trainerdex.app>
2022-03-27 13:45:33 +0000

    Get version from pyproject.toml

--- trainerdex/__init__.py
+++ trainerdex/__init__.py
@@ -4,13 +4,11 @@ Official TrainerDex API Wrapper
 
 A basic wrapper for the TrainerDex API.
 
-:copyright: (c) 2020 TurnrDev
+:copyright: (c) 2022 TurnrDev
 :licence: GNU-GPL3, see LICENSE for more details
 
 """
 
-from .version import get_version
+import pkg_resources
 
-VERSION = (4, 0, 0, "alpha", 0)
-
-__version__ = get_version(VERSION)
+__version__ = pkg_resources.get_distribution("trainerdex").version
