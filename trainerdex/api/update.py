ceb9e9e901c5ca22985d6774e1abac706589d1c0
Jay Turner <jay@trainerdex.app>
2022-10-01 01:00:52 +0100

    Import annotations

--- trainerdex/api/update.py
+++ trainerdex/api/update.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import datetime
 from decimal import Decimal
 from typing import TYPE_CHECKING
--- trainerdex/api/utils.py
+++ trainerdex/api/utils.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from inspect import isawaitable
 from typing import Any, Callable, ParamSpec, TypeVar, Union, overload
 
